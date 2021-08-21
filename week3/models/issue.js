b253c657f67b4089295cdd0c388a454e4cd0178b
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-08-20 22:33:19 -0400

    1stVote

---
+++ week3/models/issue.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose")
+const schema = mongoose.schema
+
+const issueSchema = new mongoose.Schema({
+    
+      
+    })
+
+module.exports = mongoose.model('issue',issueSchema)
---
+++ week3/models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose")
+const schema = mongoose.schema
+
+const userSchema = new mongoose.Schema({
+    
+      
+    })
+
+module.exports = mongoose.model('user',userSchema)
